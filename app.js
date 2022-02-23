b63d013c3077f0b666e80693ada5efbcece0c08d
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-02-23 12:47:53 -0500

    modified HTML for multiple projects

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
+//require statement to allow app.js to access the fs module's functions through the fs assignment
+const fs = require('fs');
+
+// access inquirer
 const inquirer = require('inquirer');
-// //require statement to allow app.js to access the fs module's functions through the fs assignment
-// const fs = require('fs');
-// // access generatePage function in page-template.js
-// const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+// access generatePage function in page-template.js
+const generatePage = require('./src/page-template.js');
 
 //Inquirer Questions Section
 // Ask questions relating to the user
@@ -33,11 +28,11 @@ const promptUser = () => {
             type: 'input',
             name: 'github',
             message: 'Enter your GitHub Username',
-            validate: nameInput => {
-                if (nameInput) {
+            validate: githubInput => {
+                if (githubInput) {
                     return true;
                 } else {
-                    console.log('Please enter your name!');
+                    console.log('Please enter your GitHub Username!');
                     return false;
                 }
             }
@@ -54,13 +49,7 @@ const promptUser = () => {
             name: 'about',
             message: 'Provide some information about yourself:',
             // when method used to conditionally prompt a question based on the user's input
-            when: ({ confirmAbout }) => {
-                if (confirmAbout) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+            when: ({ confirmAbout }) => confirmAbout
         }
     ]);
 };
@@ -81,12 +70,12 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?',
+            message: 'What is the name of your project? (Required)',
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log('Please enter your name!');
+                    console.log('You need to enter a project name!');
                     return false;
                 }
             }
@@ -95,11 +84,11 @@ const promptProject = portfolioData => {
             type: 'input',
             name: 'description',
             message: 'Provide a description of your project (Required)',
-            validate: nameInput => {
-                if (nameInput) {
+            validate: descriptionInput => {
+                if (descriptionInput) {
                     return true;
                 } else {
-                    console.log('Please enter your name!');
+                    console.log('You need to enter a project description!');
                     return false;
                 }
             }
@@ -114,11 +103,11 @@ const promptProject = portfolioData => {
             type: 'input',
             name: 'link',
             message: 'Enter the GitHub link to your project (Required)',
-            validate: nameInput => {
-                if (nameInput) {
+            validate: linkInput => {
+                if (linkInput) {
                     return true;
                 } else {
-                    console.log('Please enter your name!');
+                    console.log('You need to enter a project GitHub link!');
                     return false;
                 }
             }
@@ -146,10 +135,93 @@ const promptProject = portfolioData => {
         });
 };
 
+//Mock Data
+// const mockData = {
+//     {
+//         name: 'kaitlyn',
+//         github: 'kaitlynskinner',
+//         confirmAbout: true,
+//         about: 'I love to code!',
+//         projects: [
+//           {
+//             name: 'Run_Buddy',
+//             description: 'A website that offers fitness training services.',
+//             languages: [Array],
+//             link: 'https://kaitlynskinner.github.io/Run_Buddy/',
+//             feature: false,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Password_Generator',
+//             description: 'An application an employee can use in order to generate a random password based on criteria they have selected.',
+//             languages: [Array],
+//             link: 'https://kaitlynskinner.github.io/Password_Generator/',
+//             feature: false,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Coding_Quiz',
+//             description: 'An timed coding quiz made of multiple choice questions.',
+//             languages: [Array],
+//             link: 'https://kaitlynskinner.github.io/Coding_Quiz/',
+//             feature: false,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Robot_Gladiators',
+//             description: 'A game created to battle numerous robots, view your score, and visit a shop.',
+//             languages: [Array],
+//             link: 'https://kaitlynskinner.github.io/Robot_Gladiators/',
+//             feature: false,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Taskinator',
+//             description: 'A task tracking application',
+//             languages: [Array],
+//             link: 'https://kaitlynskinner.github.io/Taskinator/',
+//             feature: false,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Work_Day_Scheduler',
+//             description: 'A 9-5 work day scheduler with colour-coded time slots and areas users can save events to.',
+//             languages: [Array],
+//             link: 'https://kaitlynskinner.github.io/Work_Day_Scheduler/',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Weather_Dashboard',
+//             description: 'A weather dashboard where travellers can see the weather outlook for multiple cities so they are able to plan a trip accordingly.',
+//             languages: [Array],
+//             link: 'https://kaitlynskinner.github.io/Weather_Dashboard/',
+//             feature: false,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Personal_Portfolio',
+//             description: 'Professional Portfolio.',
+//             languages: [Array],
+//             link: 'https://kaitlynskinner.github.io/Personal_Portfolio/',
+//             feature: false,
+//             confirmAddProject: false
+//           }
+//         ]
+//       }
+// };
+
 // Call the function(s)
 promptUser()
 //.then(answers => console.log(answers))
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData) 
+
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
